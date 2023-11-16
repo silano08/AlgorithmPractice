1f6a4125f8fed905c66b8bc7919c1ed007103610
Won Gayeon <57881683+silano08@users.noreply.github.com>
2023-11-16 18:09:45 +0900

    2트 -> 성공 1. if문 제거하고 while문에 검사 추가 (쓸데없이 while문 돌리지 않도록) 2. if문 검사 조건 추가 힙을 뽑았는데 목표조건보다 작을시 추가검사하지않고 -1리턴하도록

--- test/com/JavaPractice/heap42626.java
+++ test/com/JavaPractice/heap42626.java
@@ -1,7 +1,8 @@
 package com.JavaPractice;
 
 import java.util.PriorityQueue;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 /*
 * 1. 아이디어
 * - while문 사용, scoville를 heap으로 만들고
@@ -18,6 +19,33 @@ import java.util.PriorityQueue;
 * */
 public class heap42626 {
 
+    @Test
+    public void testSolution() {
+        // Test Case 1
+        int[] scoville1 = {1, 2, 3, 9, 10, 12};
+        int K1 = 7;
+        int expected1 = 2;
+        assertEquals(expected1, solution(scoville1, K1));
+
+        // Test Case 2
+        int[] scoville2 = {1, 1, 1, 1};
+        int K2 = 10;
+        int expected2 = -1;
+        assertEquals(expected2, solution(scoville2, K2));
+
+        // Test Case 3
+        int[] scoville3 = {1, 3, 9, 10, 12};
+        int K3 = 7;
+        int expected3 = 1;
+        assertEquals(expected3, solution(scoville3, K3));
+
+        // Test Case 4
+        int[] scoville4 = {1, 2, 3, 4, 5};
+        int K4 = 10;
+        int expected4 = 2;
+        assertEquals(expected4, solution(scoville4, K4));
+    }
+
     public static void main(String[] args) {
         int[] scoville = {1, 2, 3, 9, 10, 12};
         int K = 7;
@@ -27,22 +55,24 @@ public class heap42626 {
 
     public static int solution(int[] scoville, int K) {
         // 스코빌 지수 힙으로 치환
-        PriorityQueue<Integer> scovilleHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int s : scoville) {
-            scovilleHeap.add(s);
+            minHeap.add(s);
         }
 
         int count = 0;
         int nowScoville = 0;
 
-        while (true){
-            if(nowScoville >= K){
-                break;
-            }else {
-                nowScoville = scovilleHeap.poll() + (scovilleHeap.poll()*2);
-                scovilleHeap.add(nowScoville);
-                count+= 1;
-            }
+        while (minHeap.size() >= 2 && minHeap.peek() < K) { // 남은 음식이 2개 이상일때만 루프돌림
+            int min1 = minHeap.poll();
+            int min2 = minHeap.poll();
+            nowScoville = min1 + (min2 * 2);
+            minHeap.add(nowScoville);
+            count++;
+        }
+
+        if(minHeap.isEmpty() || minHeap.peek() < K){
+            return -1;
         }
 
         return count;
